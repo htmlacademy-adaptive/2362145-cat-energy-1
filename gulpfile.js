b99ee1ab92c9c29a7b975a8815defd031dd7269d
Nikita7515 <152637405+Nikita7515@users.noreply.github.com>
2024-10-06 00:02:27 +0300

    начало сборки сборки

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ import less from 'gulp-less';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
+import csso from 'postcss-csso';
+import rename from 'gulp-rename';
+import htmlmin from 'gulp-htmlmin';
 
 // Styles
 
@@ -12,9 +15,11 @@ export const styles = () => {
     .pipe(plumber())
     .pipe(less())
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
 
@@ -23,7 +28,7 @@ export const styles = () => {
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -43,3 +48,17 @@ const watcher = () => {
 export default gulp.series(
   styles, server, watcher
 );
+
+// HTML
+
+export const html = () => {
+  return gulp.src('source/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('build'));
+}
+
+// Images
+
+// const images = () => {
+//   return gulp.src('source/img/**/*.{jpg,png}')
+// }
